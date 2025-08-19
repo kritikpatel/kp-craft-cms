c59be7a47c27fe1c45d6fb4e3b7aa40d117dc8ba
Kritik Patel <kritikpatel22@gmail.com>
2025-08-19 22:14:54 +0100

    Basic header main nav setup, adding navigation node

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,25 @@
 export default {
-   content: [
-    './templates/**/*.twig',
-    './src/**/*.js',
-    './src/**/*.css'
-   ],
-   theme: {
-     extend: {},
-     colors: {
-         primary: '#734B5E', // Purple
-         secondary: '#F5D3C8', // Cream
-         gray: {
-            lighter: '#BCBDC0',
-            DEFAULT: '#8A8D91',
-            darker: '#565857'
-         }
-     }
-   },
-   plugins: [],
- }
+    content: [
+        './templates/**/*.twig',
+        './src/**/*.js',
+        './src/**/*.css'
+    ],
+    theme: {
+        extend: {},
+        colors: {
+            primary: '#2f5c2d', // Green
+            secondary: '#e6d6d1', // Cream
+            gray: {
+                lighter: '#BCBDC0',
+                DEFAULT: '#8A8D91',
+                darker: '#565857'
+            },
+            white: '#FFFFFF',
+        },
+        fontFamily: {
+            primary: ['Roboto', 'sans-serif'],
+            serif: ['Merriweather', 'serif']
+        },
+    },
+    plugins: [],
+}
