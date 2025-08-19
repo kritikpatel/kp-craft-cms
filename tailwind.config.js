b8d783530a580000641e0316e1aa2f3b217238a5
Kritik Patel <kritikpatel22@gmail.com>
2025-08-19 20:52:54 +0100

    Installing Tailwind CSS, resetting index.twig template

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+export default {
+   content: [
+    './templates/**/*.twig',
+    './src/**/*.js',
+    './src/**/*.css'
+   ],
+   theme: {
+     extend: {},
+     colors: {
+         primary: '#734B5E', // Eggplant
+         secondary: '#F5D3C8', // Cream
+         gray: {
+            lighter: '#BCBDC0',
+            DEFAULT: '#8A8D91',
+            darker: '#565857'
+         }
+     }
+   },
+   plugins: [],
+ }
